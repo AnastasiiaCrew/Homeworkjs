8f59c8dcab9f0320835dfceb41583e40903cec1e
Anastasia <ainiushyna@gmail.com>
2024-06-16 14:25:39 +0300

    js homework11

--- jshomework11.js
+++ jshomework11.js
@@ -1,15 +1,3 @@
-// let array = arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-// function myBlend(arr) {
-//     let arrNumber;
-//     for (let i = 0; 1 <arr.length; i++) {
-//         let randomNumber = Math.round(Math.random() * (array.length -1 ));
-//     arrNumber = arr[randomNumber];
-//     arr[randomNumber] = arr [i];
-//     arr[i] = arrNumber;
-// }
-// }
-
 function myBlend(arr) {
     arr.forEach((_, i) => {
         const j = Math.floor(Math.random() * arr.length);
